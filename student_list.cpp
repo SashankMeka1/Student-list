3af4d603ee1402961e352e6b9a5a769bb2ea639e
Sashank <sashankmeka7@gmail.com>
2021-02-23 11:47:08 -0800

    fixes

--- student_list.cpp
+++ student_list.cpp
@@ -2,18 +2,20 @@
 #include <vector>
 #include <cstring>
 #include <cstdlib>
+#include <iomanip>
 using namespace std;
-
+int main();
 struct Student{
 	char name[20];
 	int id;//student struct
-	float gpa;
+	char* gpa;
 };
 int add(char * response, vector<Student*>*list){
+	//cast add data to struct and push back on vector
 	Student *student = new Student;
 	strcpy(student->name, strtok(response, ","));
 	student->id = atoi(strtok(NULL, " ,"));
-	student->gpa = strtof(strtok(NULL, " ,"), NULL);
+	student->gpa = strtok(NULL, " ,");
 	list->push_back(student);
 	return main();
 }
@@ -25,15 +27,17 @@ int del(char * response, vector<Student*>*list){
 			delete *i;
 			list->erase(i);
 			cout << "Deleted"<<endl;
+			cin.ignore(100, '\n');
 			return main();
 		}
 	}
 	cout << "Student not found"<<endl;
+	cin.ignore(100, '\n');
 	return main();
 }
 int print(char * response, vector<Student*>*list){
 	for(auto i = list->begin(); i !=list->end();++i){
-		cout<<"Name, "<<(*i)->name<<" ID, "<<(*i)->id<<" GPA, "<<(*i)->gpa<<endl;
+		cout<<"Name, "<<(*i)->name<<" ID, "<<(*i)->id<<" GPA, "<<setprecision(3)<< (*i)->gpa<<endl;
 	}//loop through and print
 	return main();	
 }
@@ -41,26 +45,27 @@ int main(){
 	static char response[100];//static for memory conservation
 	static vector<Student*>*list = new vector<Student*>;//static so list is same
 	cout << "ADD DELETE PRINT or QUIT. Caps lock only"<<endl;
-	cin.getline(response,100);//get input
+	cin >> response;//get input
+	cin.ignore(100, '\n');
+	cin.clear();
 	if(strcmp(response, "ADD")==0){
 		cout<< "Give student information in format:name,id,gpa.NO SPACES except in name."<<endl;
 		cin.getline(response,100);//get student data
-		if(strchr(strchr(response, ' ')+1, ' ')){
+		if(strchr(strchr(response, ' ')+1, ' ')){//keep out the spaces
 			cout << "NO SPACES except in name"<<endl;
 			return main();
 		}
 		else{//take token of data and add to list
-			add(response, list);
+			return add(response, list);
 		}
 	}
 	else if(strcmp(response, "DELETE")==0){
-		del(response, list);
+		return del(response, list);
 	}
 	else if(strcmp(response, "PRINT")==0){
-		print(response, list);
+		return print(response, list);
 	}
 	else if(strcmp(response, "QUIT")==0){
-		delete list;//dynamic so delete and end
 		return 0;
 	}
 	cout << "Enter a valid command"<<endl;
